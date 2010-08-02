be704d6be99e61913fb436a29109b726208c07be
eg.public <eg.public@4857f41f-a0ac-a539-133c-166ed2097b4f>
2010-08-02 07:21:03 +0000

    I don't like windows...

--- trunk/src/qcommon/net_ip.c
+++ trunk/src/qcommon/net_ip.c
@@ -322,7 +322,8 @@ static qboolean Sys_StringToSockaddr(const char *s, struct sockaddr *sadr, int s
 			Com_Printf("Sys_StringToSockaddr: Error resolving %s: No address of required type found.\n", s);
 	}
 	else
-		Com_Printf("Sys_StringToSockaddr: Error resolving %s: %s\n", s, gai_strerror(retval));
+		// Doesn't play well with old machines?
+		//Com_Printf("Sys_StringToSockaddr: Error resolving %s: %s\n", s, gai_strerror(retval));
 	
 	if(res)
 		freeaddrinfo(res);
